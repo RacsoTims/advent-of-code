b4a4a379aaa78bf7e1cdb4241e535b46b132e071
Oscar Smit <oscar-850@hotmail.com>
2025-01-21 09:12:12 +0100

    2016-puzzles

--- 2016/day9.py
+++ 2016/day9.py
@@ -4,24 +4,25 @@
 import re
 
 path = "C:\\Users\\oscar\\my_stuff\\advent-of-code\\2016\\day9_input.txt"
-marker_pattern = r'\((\d+)x(\d+)\)'
+pattern = r'[A-Z]|\(\d+x\d+\)'
+marker_pattern = r'(\d+)x(\d+)'
 
 with open(path, 'r') as puzzle_input:
-    data = [x.removesuffix("\n") for x in puzzle_input.readlines()]
+    data = puzzle_input.read()
 
-for datum in data:
-    capture = False
-    decompressed_string = ""
-    for i in range(len(datum)):
-        if not capture:
-            decompressed_string += datum[i]
-        elif datum[i] == "(" and not capture:
-            capture = True
-            marker = re.search(marker_pattern)
-            marker_length = len(marker.group(0))
-            subsequent_chars = marker.group(1)
-            repeat = marker.group(2)
-        else:
-            continue
-    print(decompressed_string)
-# print(data)
+decompressed_string = ""
+parts = re.findall(pattern, data)
+i = 0
+while i < len(parts):
+    if len(parts[i]) == 1:
+        decompressed_string += parts[i]
+        i += 1
+    else:
+        match_numbers = re.search(marker_pattern, parts[i])
+        capture_length = int(match_numbers[1])
+        repeat = int(match_numbers[2])
+        captured_string = "".join(parts[i+1:i+1+capture_length])
+        decompressed_string += captured_string[:capture_length] * repeat
+        corrected_length = len(re.sub(r'\(\d+x\d+\)', "?", captured_string[:capture_length]))
+        i += corrected_length + 1
+print(len(decompressed_string))
