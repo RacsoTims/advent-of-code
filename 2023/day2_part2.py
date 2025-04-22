c288ebe57d4a71365b3e5d1352da6a767a7eaf54
RacsoTims <oscar-850@hotmail.com>
2025-04-22 12:50:47 +0200

    2023-puzzles

--- 2023/day2_part2.py
+++ 2023/day2_part2.py
@@ -1,12 +1,21 @@
 # URL:		https://adventofcode.com/2023/day/2#part2
-# Answer:	0
+# Answer:	65371
 
 import os
+import re
 puzzle_input = 'C:\\Users\\oscar\\my_stuff\\advent-of-code\\2023\\day2_input.txt'
 example_input = 'C:\\Users\\oscar\\my_stuff\\advent-of-code\\2023\\day2_example.txt'
 if os.name == 'posix':
 	puzzle_input = '/home/oscar/projects/advent-of-code/2023/day2_input.txt'
 	example_input = '/home/oscar/projects/advent-of-code/2023/day2_example.txt'
 
-with open(example_input, 'r') as data:
-	pass
+sum_powers = 0
+
+with open(puzzle_input, 'r') as data:
+	for game in data.readlines():
+		game_ID = int(re.match(r'Game (\d+)', game)[1])
+		reds = list(map(int, re.findall(r'(\d+) red', game)))
+		greens = list(map(int, re.findall(r'(\d+) green', game)))
+		blues = list(map(int, re.findall(r'(\d+) blue', game)))
+		sum_powers += max(reds) * max(greens) * max(blues)
+print(sum_powers)
--- 2023/day3.py
+++ 2023/day3.py
@@ -2,11 +2,45 @@
 # Answer:	0
 
 import os
+import numpy as np
 puzzle_input = 'C:\\Users\\oscar\\my_stuff\\advent-of-code\\2023\\day3_input.txt'
 example_input = 'C:\\Users\\oscar\\my_stuff\\advent-of-code\\2023\\day3_example.txt'
 if os.name == 'posix':
 	puzzle_input = '/home/oscar/projects/advent-of-code/2023/day3_input.txt'
 	example_input = '/home/oscar/projects/advent-of-code/2023/day3_example.txt'
 
-with open(example_input, 'r') as data:
-	pass
+sum_parts = 0
+
+def check_submatrix(i,j, grid) -> bool:
+	result = False
+	directions = [(-1, -1), (-1, 0), (-1, 1), (0,-1), (0,1), (1, -1), (1, 0), (1,1)]
+	for x,y in directions:
+		try:
+			cell = grid[i+x][j+y]
+			if not cell.isdigit() and cell != ".":
+				result = True
+		except(IndexError):
+			continue
+	return result
+
+
+with open(puzzle_input, 'r') as data:
+	lines = [x.removesuffix("\n") for x in data.readlines()]
+
+grid = np.array([list(line) for line in lines])
+numbers = []
+
+for i in range(len(grid)):
+	number = []
+	status = []
+	for j in range(len(grid[i])):
+		if grid[i][j].isdigit():
+			number.append(grid[i][j])
+			status.append(check_submatrix(i, j, grid))
+		else:
+			if len(number) > 0 and True in status:
+				numbers.append(number)
+			number = []
+			status = []
+
+print(sum([int("".join(x)) for x in numbers]))
