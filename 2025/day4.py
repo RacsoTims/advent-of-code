8d0bcf8279f46ce8639fadf4924f184f65249da0
RacsoTims <oscar-850@hotmail.com>
2025-12-08 08:12:15 +0100

    2025 puzzles

--- 2025/day4.py
+++ 2025/day4.py
@@ -1,6 +1,7 @@
 # URL:		https://adventofcode.com/2025/day/4
 # Answer:	0
 
+import numpy
 import os
 puzzle_input = '.\\day4_input.txt'
 example_input = '.\\day4_example.txt'
@@ -8,5 +9,37 @@ if os.name == 'posix':
 	puzzle_input = './day4_input.txt'
 	example_input = './day4_example.txt'
 
-with open(example_input, 'r') as data:
-	pass
+def count_neighbours(coordinates, constraint) -> bool:
+	accessible = True
+	count = 0
+	for row in range(-1, 2):
+		for col in range(-1, 2):
+			y = coordinates[0] + row
+			x = coordinates[1] + col
+			if not (-1 < x < 136) or not (-1 < y < 136):
+				continue
+			cell = grid[y][x]
+			if cell == "@" and not (row == 0 and col == 0):
+				count += 1
+			if count >= constraint:
+				accessible = False
+				break
+	return accessible
+
+grid = numpy.zeros((136, 136), dtype=str)
+constraint = 4
+access = 0
+
+with open(puzzle_input, 'r') as data:
+	lines = [l.removesuffix("\n") for l in data.readlines()]
+	for i in range(len(lines)):
+		for j in range(len(lines[i])):
+			grid[i][j] = lines[i][j]
+
+for i in range(len(grid)):
+	for j in range(len(grid[i])):
+		if grid[i][j] == ".":
+			continue
+		elif count_neighbours([i, j], constraint):
+			access += 1
+print(access)
--- 2025/day6.py
+++ 2025/day6.py
@@ -1,6 +1,8 @@
 # URL:		https://adventofcode.com/2025/day/6
 # Answer:	0
 
+import re
+import math
 import os
 puzzle_input = '.\\day6_input.txt'
 example_input = '.\\day6_example.txt'
@@ -8,5 +10,17 @@ if os.name == 'posix':
 	puzzle_input = './day6_input.txt'
 	example_input = './day6_example.txt'
 
-with open(example_input, 'r') as data:
-	pass
+grand_total = 0
+
+with open(puzzle_input, 'r') as data:
+	lines = [l.removesuffix("\n").strip() for l in data.readlines()]
+	first_terms = re.split(r"\s+", lines[0])
+	for i in range(len(first_terms)):
+		terms = [first_terms[i]]
+		for j in range(1, len(lines)):
+			terms.append(re.split(r"\s+", lines[j])[i])
+		if "+" in terms:
+			grand_total += sum(map(int, terms[:-1]))
+		else:
+			grand_total += math.prod(map(int, terms[:-1]))
+print(grand_total)
