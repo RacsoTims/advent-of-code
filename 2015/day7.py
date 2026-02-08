91ce4549373d7cde458a822fc4d7cbc0e1d66217
Oscar Smit <oscar-850@hotmail.com>
2026-02-08 10:56:59 +0100

    advent_2015

--- 2015/day7.py
+++ 2015/day7.py
@@ -12,6 +12,7 @@ if os.name == 'posix':
 
 bits = 16
 wires = {}
+live_wires = []
 
 def calculate_bitwise_OR(m, n):
     bin_m = format(int(m), '016b')
@@ -50,26 +51,48 @@ def calculate_left_shift(n, shift):
 
 
 def assign_value_directly(groups):
-    wires[groups[1]] = groups[0]
+    if r"\d+" not in groups[0]:
+        if groups[0] not in wires:
+            wires[groups[0]] = 0
+        wires[groups[1]] = wires[groups[0]]
+    else:
+        wires[groups[1]] = groups[0]
 
 
 def complement(groups):
+    if groups[0] not in wires:
+        wires[groups[0]] = 0
     wires[groups[1]] = calculate_bitwise_complement(wires[groups[0]])
 
 
 def conjunction(groups):
-    wires[groups[2]] = calculate_bitwise_AND(wires[groups[0]], wires[groups[1]])
+    if groups[0] == 1:
+        if groups[1] not in wires:
+            wires[groups[1]] = 0
+        wires[groups[2]] = calculate_bitwise_AND(groups[0], wires[groups[1]])
+    else:
+        for wire in [groups[0], groups[1]]:
+            if wire not in wires:
+                wires[wire] = 0
+        wires[groups[2]] = calculate_bitwise_AND(wires[groups[0]], wires[groups[1]])
 
 
 def disjunction(groups):
+    for wire in [groups[0], groups[1]]:
+        if wire not in wires:
+            wires[wire] = 0
     wires[groups[2]] = calculate_bitwise_OR(wires[groups[0]], wires[groups[1]])
 
 
 def left_shift(groups):
+    if groups[0] not in wires:
+        wires[groups[0]] = 0
     wires[groups[2]] = calculate_left_shift(wires[groups[0]], groups[1])
 
 
 def right_shift(groups):
+    if groups[0] not in wires:
+        wires[groups[0]] = 0
     wires[groups[2]] = calculate_right_shift(wires[groups[0]], groups[1])
 
 
@@ -78,7 +101,7 @@ def execute_instruction(instruction):
         result = re.search(r"NOT\s([a-z]+)\s->\s([a-z]+)", instruction).groups()
         complement(result)
     elif "AND" in instruction:
-        result = re.search(r"([a-z]+)\sAND\s([a-z]+)\s->\s([a-z]+)", instruction).groups()
+        result = re.search(r"([a-z]+|1)\sAND\s([a-z]+)\s->\s([a-z]+)", instruction).groups()
         conjunction(result)
     elif "OR" in instruction:
         result = re.search(r"([a-z]+)\sOR\s([a-z]+)\s->\s([a-z]+)", instruction).groups()
@@ -89,15 +112,20 @@ def execute_instruction(instruction):
     elif "RSHIFT" in instruction:
         result = re.search(r"([a-z]+)\sRSHIFT\s(\d+)\s->\s([a-z]+)", instruction).groups()
         right_shift(result)
+    elif len(re.findall(r"^[a-z]+", instruction)) > 0:
+        result = re.search(r"([a-z]+)\s->\s([a-z]+)", instruction).groups()
+        assign_value_directly(result)
     else:
         result = re.search(r"(\d+)\s->\s([a-z]+)", instruction).groups()
         assign_value_directly(result)
 
 
 with open(puzzle_input, 'r') as data:
-	for instruction in data.readlines():
-		execute_instruction(instruction)
-print(wires)
+    for instruction in data.readlines():
+        # print(instruction)
+        execute_instruction(instruction)
+wires_sorted = {k: v for k, v in sorted(wires.items(), key=lambda item: item[0])}
+print((wires_sorted))
 print(wires['a'])
 
 # signal to wire            (\d+)\s->\s([a-z]+)
